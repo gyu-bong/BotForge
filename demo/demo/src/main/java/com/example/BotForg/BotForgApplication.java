c7655f4d521b3668a97a06015f74695b35028639
gyu-bong <lg.bong94@gmail.com>
2024-09-30 14:36:21 +0900

    Add files via upload

---
+++ demo/demo/src/main/java/com/example/BotForg/BotForgApplication.java
@@ -0,0 +1,13 @@
+package com.example.BotForg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotForgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotForgApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/example/BotForg/BotForgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BotForg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotForgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
